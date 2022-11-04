8d0f2c194cf9fd65f7c0f7515de9f47f1652f719
Marion <mrobert148@gmail.com>
2022-11-04 12:15:20 +0100

    challenges from 1 to 9

--- app.js
+++ app.js
@@ -15,8 +15,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+  res.render("home", {pageContent: homeStartingContent});
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {pageContent: aboutContent});
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact", {pageContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  console.log(req.body.postTitle);
+});
 
 
 
